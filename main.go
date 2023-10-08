6c89fb9ed33acd77f198f275ee30a6f3368584e5
darkoli <darkoli@tencent.com>
2023-10-08 20:30:29 +0800

    feat: update code

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Timestamp int64
+
+// 短链接域名
+const Base_Domain = "https://example.com"
+
+type Link struct {
+	SourceLink    string
+	ShortLink     string
+	GeneratedTime Timestamp
+	VisitCount    int
+	MaxVisitCount int
+	Expired       Timestamp
+}
+
+var links []Link
+
+// 生成一个唯一 id 用于短链接匹配
+func GenerateUUID() {
+
+}
 
 func main() {
-	fmt.Println("Hello Tiny-URL")
+	// fmt.Println("Hello Tiny-URL")
+	fmt.Printf(" short link %v\n", links)
 }
